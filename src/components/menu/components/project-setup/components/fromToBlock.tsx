9c6328f87352aa34576c3a5eb322eaeba566310b
Sofitt <230320011q@gmail.com>
2023-02-26 14:28:15 +0700

    Добавил настройку окружения

--- src/components/menu/components/project-setup/components/fromToBlock.tsx
+++ src/components/menu/components/project-setup/components/fromToBlock.tsx
@@ -1,6 +1,7 @@
 import React, {Dispatch, useState} from "react";
 import {styleParser} from "../../../../../lib/styleParser";
 import {numberMask} from "../../../../../lib/numberMask";
+import '../../../../../assets/styles/from-to-block.css'
 
 interface mode {
     mode: string
@@ -12,8 +13,8 @@ const mods:Array<mode> = [
 ]
 
 function DistanceBlock ({dValue}:{dValue:number}) {
-    const [options, setOptions]:any = useState({})
-    const [editMode, setEdit]:any = useState(false)
+    const [options, setOptions]:[object|any, Dispatch<object>] = useState({})
+    const [editMode, setEdit]:[boolean, Dispatch<boolean>] = useState(false)
     const composedOptions = (transformDisplay?: boolean | undefined) => {
         // при сохранении в объект настроек дублирующие поля перезаписывают старые. Поэтому не нужно дополнительно проверять повторы
         const keys = Object.keys(options)
@@ -28,10 +29,9 @@ function DistanceBlock ({dValue}:{dValue:number}) {
     }
     const updateOptions = (evt: React.FormEvent<HTMLTextAreaElement>) => {
         const input:string|string[] = (evt.target as HTMLInputElement).value
-        // todo оставить комментарий в ui, что сепаратор выражений - ";"
         const result = styleParser(input)
         if (!result) return
-        setOptions(result)
+        setOptions(result as object)
     }
     const updateEdit = () => {
         setEdit(!editMode)
@@ -44,10 +44,10 @@ function DistanceBlock ({dValue}:{dValue:number}) {
             <textarea onInput={(evt) => updateOptions(evt)} defaultValue={composedOptions()}></textarea>
         </>
         : <p onClick={updateEdit}>
-            {`${dValue}: `}{`[${composedOptions(true)}]`}
+            {`${dValue}: `}{`[ ${composedOptions(true)} ]`}
         </p>
     return (
-        <div className='distanceBlock'>
+        <div className='distance-block'>
             { content }
         </div>
     )
@@ -56,7 +56,7 @@ function DistanceBlock ({dValue}:{dValue:number}) {
 const AddBlock = (props: { addBlock: (arg0: number) => void; }):JSX.Element => {
     const [isInputShow, showInput] = useState(false)
     const [inputValue, setInputValue] = useState('')
-    const updateInputValue = (evt:Event) => {
+    const updateInputValue = (evt:React.FormEvent) => {
         const value = (evt.target as HTMLInputElement).value
         const masked = numberMask(value)
         if (masked === false) {
@@ -64,21 +64,21 @@ const AddBlock = (props: { addBlock: (arg0: number) => void; }):JSX.Element => {
         }
         setInputValue(masked as string)
     }
-    const add = (evt:KeyboardEvent) => {
+    const add = (evt:React.KeyboardEvent) => {
         if (evt.key !== 'Enter') {
             return
         }
         showInput(false)
         props.addBlock(+inputValue)
     }
-    const input = <input type="text" onKeyDown={add} onInput={updateInputValue} value={inputValue}/>
-    const button = <button onClick={() => showInput(true)}>+</button>
+    const input = <input className='add-block_input' placeholder='Шаг в процентах (0.01-99.99)' type="text" onKeyDown={add} onInput={updateInputValue} value={inputValue}/>
+    const button = <button className='add-block' onClick={() => showInput(true)}>+</button>
     return (
         isInputShow ? input : button
     )
 }
 
-const block = (addButton:boolean):JSX.Element => {
+const Blocks = (props: { addButton: boolean }):JSX.Element => {
     const list = [0, 100]
     const [blocks, updateBlocks] = useState(list)
     const handleUpdateBlocks = (block: number) => {
@@ -93,44 +93,71 @@ const block = (addButton:boolean):JSX.Element => {
         return <DistanceBlock dValue={+block} key={i}/>
     })
     return (
-        <div className="block">
+        <div className='blocks'>
             {distanceBlocks}
-            {/* todo кнопка добавления блока шага */}
-            {addButton && <AddBlock addBlock={handleUpdateBlocks}/>}
+            {props.addButton && <AddBlock addBlock={handleUpdateBlocks}/>}
+        </div>
+    )
+}
+interface noticeData {
+    toggleFunc: () => void,
+    state: boolean
+}
+interface btns {
+    mode: string,
+    setMode: React.Dispatch<string>,
+    noticeData: noticeData
+}
+
+const Actions = (props:btns):JSX.Element => {
+    return (
+        <div className="actions">
+            {
+                mods.map((data, i) => {
+                    return <button onClick={() => props.setMode(data.mode)}
+                                   key={i}
+                                   className={props.mode === data.mode ? 'active' : ''}
+                    >+</button>
+                })
+            }
+            <button onClick={props.noticeData.toggleFunc}
+                    className={`show-notice ${props.noticeData.state ? 'active' : ''}`}>?</button>
         </div>
     )
 }
 
-const btns = (mode:string, setMode:Dispatch<string>):JSX.Element => {
+const NoticeBlock = ():JSX.Element => {
+    const notices = [
+        'Сепаратор выражений - ";"',
+        'При дублировании стилей записывается последний написанный'
+    ]
     return (
-        <div className="btns">
-            {
-                mods.map((data, i) => {
-                    return <button onClick={() => setMode(data.mode)}
-                                   key={i}
-                                   className={mode === data.mode ? 'active' : ''}
-                    >+</button>
-                })
-            }
-        </div>
+        <ol className='notice-block'>
+            {notices.map((notice, i) => {
+                return (
+                    <li key={i}>{`${i+1}.`} {notice}</li>
+                )
+            })}
+        </ol>
     )
 }
 
 const fromToBlock = () => {
     const [mode, setMode] = useState('percent')
-    const headerClasses = `header __${mode}`
+    const [notice, showNotice] = useState(false)
+    const toggleNotice = () => {
+        showNotice(!notice)
+    }
     return (
-        <div>
-            <div className={headerClasses}>
+        <div className='from-to-block'>
+            <div className='header'>
                 <h2>From-to</h2>
-                {btns(mode, setMode)}
+                <Actions mode={mode}
+                         setMode={setMode}
+                         noticeData={{toggleFunc: toggleNotice, state: notice}}/>
             </div>
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <span className='notice'>Сепаратор выражений - ";"</span>
-            {/* todo дописать подсказку, что при повторении стилей записывается последняя найденная строка*/}
-            {
-                block(mode === 'value')
-            }
+            {notice && <NoticeBlock/>}
+            <Blocks addButton={mode === 'value'}/>
         </div>
     )
 }
